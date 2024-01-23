0063bf7751c45793d1d392e15cee88b74f753216
Jarvs06 <104308110+Jarvs06@users.noreply.github.com>
2024-01-23 19:35:35 +0800

    official receipt

--- src/receipt-transactions-home.jsx
+++ src/receipt-transactions-home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./styles.css";
 
 const styles = {
   container: {
-    width: 'auto',
+    width: '500px',
     height: 'auto',
     margin: 'auto',
     fontFamily: 'Arial, sans-serif',
@@ -10,37 +11,71 @@ const styles = {
   },
   header: {
     textAlign: 'center',
-    fontSize: '16px',
+    fontSize: '22px',
     fontWeight: 'bold',
     marginBottom: '8px',
   },
-  content: {
-    fontSize: '12px',
-    marginBottom: '10px',
+  offReceipt: {
+    textAlign: 'center',
+    fontSize: '18px',
+    fontWeight: 'bold',
   },
+  content: {
+    fontSize: '13px',
+  },
+
+  table: {
+    width: '100%',
+  },
+
+  tableQty: {
+    width: '10%',
+  },
+  tableDesc: {
+    width: '65%',
+  },
+  tableAmount: {
+    width: '25%',
+    textAlign: 'right',
+  },
+
 };
 
 const Receipt = React.forwardRef(({ transactionData }, ref) => {
   return (
       <div style={styles.container} ref={ref}>
-          <div style={styles.header}>Transaction Details</div>
+          <div style={styles.header}>Melyson Enterprise</div>
+          <p style={styles.offReceipt}>OFFICIAL RECEIPT</p>
+          <span className='receiptDivider'></span>
           
-          {/* Use map to create <div> elements based on the length of transactionData */}
-          <table>
-          {transactionData && (          
-                <tr>
-                  <td>{transactionData.itemQuantity}</td>
-                  <td>{transactionData.itemName}</td>
-                  <td>{transactionData.itemPrice}</td>
-                </tr> 
-          )}
-          </table>
+          {/* Use map to create <table> based on the length of productBreakdown */}
+        <table className='receiptTable'>
+          <tbody>
+            <tr>
+              <td style={styles.tableQty}>Qty</td>
+              <td style={styles.tableDesc}>Description</td>
+              <td style={styles.tableAmount}>Amount PHP</td>
+            </tr>
+            <tr><td colSpan={3}><span className='receiptDivider'></span></td></tr>
+            {transactionData?.productBreakdown && transactionData.productBreakdown.map((product, index) => (
+              <tr key={index}>
+                <td>{product.itemQuantity}</td>
+                <td>{product.itemName} @{product.itemPrice}</td>
+                <td style={styles.tableAmount}>₱{product.itemQuantity * product.itemPrice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <span className='receiptDivider last'></span>
           
           {transactionData && (
               <>
-                  <div style={styles.content}>Transaction Date: {transactionData.dateTransaction}</div>
-                  <div style={styles.content}>Transaction ID: {transactionData.referenceId}</div>
-                  <div style={styles.content}>Grand Amount: {transactionData.totalAmount}</div>
+                  <p style={styles.content}>Transaction Date: {transactionData.dateTransaction}</p>
+                  <p style={styles.content}>Reference ID: {transactionData.referenceId}</p>
+                  <p style={styles.content}>Total Amount: ₱{transactionData.totalAmount.toFixed(2)}</p>
+                  <p style={styles.content}>Cash: ₱{transactionData.totalAmount.toFixed(2)}</p>
+                  <p style={styles.content}>Change: ₱{transactionData.totalAmount.toFixed(2)}</p>
               </>
           )}
       </div>
